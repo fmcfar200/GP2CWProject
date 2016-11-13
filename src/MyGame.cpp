db3fa00d97ebdd03f55dcb5cbdf3011e647b557c
fmcfar200 <fmcfar200@caledonian.ac.uk>
2016-11-13 01:10:28 +0000

    Additions
    Added MyGame Game Application Common

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -1,2 +1,27 @@
 #include "MyGame.h"
+#include "Common.h"
+#include "..\include\MyGame.h"
 
+MyGame::MyGame()
+{
+}
+
+MyGame::~MyGame()
+{
+}
+
+void MyGame::render()
+{
+}
+
+void MyGame::update()
+{
+}
+
+void MyGame::initScene()
+{
+}
+
+void MyGame::destroyScene()
+{
+}
