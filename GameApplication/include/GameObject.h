9ff3fd6253c4f3720fcdcf1b0193269f9a872170
fmcfar200 <fmcfar200@caledonian.ac.uk>
2016-11-15 23:55:51 +0000

    Added Texture,GameObject & Shader.cpp .h

---
+++ GameApplication/include/GameObject.h
@@ -0,0 +1,54 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include "Common.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "Vertex.h"
+
+class GameObject
+{
+public:
+	GameObject();
+	~GameObject();
+
+	void onRender(mat4& view, mat4& projection);
+	void onUpdate();
+	void onInit();
+	void onDestroy();
+
+	void loadTexture(const string& filename);
+	void loadShaders(const string& vsFilename, const string& fsFilename);
+	void copyVertexData(Vertex *pVerts, int numberOfVertcies, int * indices, int numberOfIndices);
+
+private:
+	GLuint m_VBO;
+	GLuint m_VAO;
+	GLuint m_IBO;
+	std::vector<unsigned int> indices;
+	int m_NumberOfIndices;
+
+
+	//Shader Program
+	GLuint m_ShaderProgram;
+
+	mat4 m_ModelMatrix;
+
+	GLuint m_Texture;
+	GLuint m_ClampSampler;
+
+	vec3 m_Position;
+	vec3 m_Rotation;
+	vec3 m_Scale;
+
+
+
+	mat4 m_TranslationMatrix;
+	mat4 m_RotationMatrix;
+	mat4 m_ScaleMatrix;
+
+	int m_NumberOfVertices;
+protected:
+};
+
+#endif
