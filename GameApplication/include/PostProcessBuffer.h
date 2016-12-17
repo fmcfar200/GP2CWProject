9a20a037a4fe4ceabad1cb8519dd391b46725886
JackOswald <jack_oswald_cfc@hotmail.com>
2016-12-17 22:31:57 +0000

    Added PostProcessBuffer

---
+++ GameApplication/include/PostProcessBuffer.h
@@ -0,0 +1,29 @@
+#ifndef POSTPROCESSBUFFER_H
+#define POSTPROCESSBUFFER_H
+
+#include "Common.h"
+
+class PostProcessBuffer
+{
+public:
+	PostProcessBuffer();
+	~PostProcessBuffer();
+
+	bool create(unsigned int width, unsigned int height);
+	void destroy();
+
+	void bind();
+	void unbind();
+
+	GLuint GetTexture()
+	{
+		return m_Texture;
+	};
+private:
+	GLuint m_Texture;
+	GLuint m_Sampler;
+	GLuint m_DepthBuffer;
+	GLuint m_Framebuffer;
+};
+
+#endif
